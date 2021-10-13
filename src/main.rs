4a898c6c5ce320d99f13b5fd3d34ec3b95896d48
Megan Pycroft <megan.pycroft@gmail.com>
2021-10-13 19:31:59 +1100

    Update projectile calculator to show trajectory on a canvas

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use raytracer::math::{Point, Vector};
+use raytracer::{
+    math::{Point, Vector},
+    Canvas, Colour,
+};
+use std::fs::write;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Projectile {
@@ -30,25 +34,42 @@ impl Environment {
 }
 
 fn main() {
-    let mut proj =
-        Projectile::new(Point::new(0.0, 1.0, 0.0), Vector::new(1.0, 1.0, 0.0));
+    let width: i32 = 900;
+    let height: i32 = 500;
+    let mut canvas = Canvas::new(width as usize, height as usize);
+
+    let mut proj = Projectile::new(
+        Point::new(0.0, 1.0, 0.0),
+        Vector::new(1.0, 1.8, 0.0).normalise() * 11.25,
+    );
 
     let env = Environment::new(
         Vector::new(0.0, -0.1, 0.0),
         Vector::new(-0.01, 0.0, 0.0),
     );
 
-    let mut tick: u32 = 0;
-
     println!("Initial projectile {:?}, environment {:?}", proj, env);
 
+    let mut tick: u32 = 0;
+
     while proj.position.y >= 0.0 {
+        let x = proj.position.x as i32;
+        let y = height - proj.position.y as i32;
+
+        if (0..width).contains(&x) && (0..height).contains(&y) {
+            canvas.write_pixel(
+                x as usize,
+                y as usize,
+                Colour::new(1.0, 1.0, 1.0),
+            );
+        }
+
         proj.update(&env);
 
         tick += 1;
-
-        println!("Tick {}, projectile {:?}", tick, proj);
     }
 
     println!("\nProjectile landed in {} ticks", tick);
+
+    write("image.ppm", canvas.to_ppm()).unwrap()
 }
