b6c7af17e036c1a181b4001f3cd15bad6403d36a
Megan Pycroft <megan.pycroft@gmail.com>
2021-10-15 17:04:11 +1100

    Experiment with matrices

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,21 @@
-use raytracer::{
-    math::{Point, Vector},
-    Canvas, Colour,
-};
-use std::fs::write;
-
-#[derive(Clone, Copy, Debug)]
-pub struct Projectile {
-    pub position: Point,
-    pub velocity: Vector,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct Environment {
-    pub gravity: Vector,
-    pub wind: Vector,
-}
-
-impl Projectile {
-    pub fn new(position: Point, velocity: Vector) -> Self {
-        Self { position, velocity }
-    }
-
-    pub fn update(&mut self, environment: &Environment) {
-        self.position += self.velocity;
-        self.velocity += environment.gravity + environment.wind;
-    }
-}
-
-impl Environment {
-    pub fn new(gravity: Vector, wind: Vector) -> Self {
-        Self { gravity, wind }
-    }
-}
+use raytracer::math::{Matrix, Point};
 
 fn main() {
-    let width: i32 = 900;
-    let height: i32 = 500;
-    let mut canvas = Canvas::new(width as usize, height as usize);
+    let mut id = Matrix::identity();
+    println!("{:?}", id.invert().unwrap());
 
-    let mut proj = Projectile::new(
-        Point::new(0.0, 1.0, 0.0),
-        Vector::new(1.0, 1.8, 0.0).normalise() * 11.25,
-    );
+    let m = Matrix::new([
+        [1.0, 2.0, 3.0, 4.0],
+        [2.5, 0.5, 3.5, 1.5],
+        [2.0, -1.0, -2.0, -3.0],
+        [5.0, 6.0, 7.0, 8.0],
+    ]);
+    println!("{:?}", m * m.invert().unwrap());
 
-    let env = Environment::new(
-        Vector::new(0.0, -0.1, 0.0),
-        Vector::new(-0.01, 0.0, 0.0),
-    );
+    println!("{:?}", m.transpose().invert().unwrap());
+    println!("{:?}", m.invert().unwrap().transpose());
 
-    println!("Initial projectile {:?}, environment {:?}", proj, env);
+    id[1][2] = 3.0;
 
-    let mut tick: u32 = 0;
-
-    while proj.position.y >= 0.0 {
-        let x = proj.position.x as i32;
-        let y = height - proj.position.y as i32;
-
-        if (0..width).contains(&x) && (0..height).contains(&y) {
-            canvas.write_pixel(
-                x as usize,
-                y as usize,
-                Colour::new(1.0, 1.0, 1.0),
-            );
-        }
-
-        proj.update(&env);
-
-        tick += 1;
-    }
-
-    println!("\nProjectile landed in {} ticks", tick);
-
-    write("image.ppm", canvas.to_ppm()).unwrap()
+    println!("{:?}", id * Point::new(1.0, 2.0, 3.0));
 }
--- src/math/mod.rs
+++ src/math/mod.rs
@@ -8,5 +8,6 @@ mod matrix;
 mod point;
 mod vector;
 
+pub use matrix::Matrix;
 pub use point::Point;
 pub use vector::Vector;
