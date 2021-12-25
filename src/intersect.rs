794ba82fa868319fb90819f919297cf2dabdb569
Megan Pycroft <megan.pycroft@gmail.com>
2021-12-26 09:38:03 +1100

    Add Computations struct and add prepare_computations function to Intersection

--- src/intersect.rs
+++ src/intersect.rs
@@ -2,7 +2,10 @@ use std::ops::{Deref, DerefMut};
 
 use derive_more::Constructor;
 
-use crate::{math::Ray, Sphere};
+use crate::{
+    math::{Point, Ray, Vector},
+    Sphere,
+};
 
 /// A trait that objects need to implement if they can be intersected in a
 /// scene, returns a vector of intersection t values.
@@ -16,6 +19,24 @@ pub trait Intersectable {
 pub struct Intersection<'a> {
     pub object: &'a Sphere,
     pub t: f64,
+}
+
+impl<'a> Intersection<'a> {
+    pub fn prepare_computations(&self, ray: &Ray) -> Computations<'a> {
+        let point = ray.position(self.t);
+        let eye = -ray.direction;
+        let mut normal = self.object.normal_at(&point);
+
+        let inside = if eye.dot(&normal) < 0.0 {
+            normal = -normal;
+
+            true
+        } else {
+            false
+        };
+
+        Computations::new(self.object, self.t, point, eye, normal, inside)
+    }
 }
 
 /// An IntersectionList is a simple wrapper around a vector of Intersections, it
@@ -70,6 +91,18 @@ impl<'a> DerefMut for IntersectionList<'a> {
     }
 }
 
+/// The Computations struct is a helper structure to store precomputed values
+/// about an intersection.
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Constructor)]
+pub struct Computations<'a> {
+    pub object: &'a Sphere,
+    pub t: f64,
+    pub point: Point,
+    pub eye: Vector,
+    pub normal: Vector,
+    pub inside: bool,
+}
+
 #[cfg(test)]
 mod tests {
     use approx::*;
@@ -84,6 +117,32 @@ mod tests {
 
         assert_relative_eq!(*i.object, s);
         assert_float_relative_eq!(i.t, 3.5);
+    }
+
+    #[test]
+    fn prepare_computations() {
+        let r = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::z_axis());
+        let s = Sphere::default();
+        let i = Intersection::new(&s, 4.0);
+
+        let c = i.prepare_computations(&r);
+
+        assert_float_relative_eq!(c.t, i.t);
+        assert_relative_eq!(c.object, i.object);
+        assert_relative_eq!(c.point, Point::new(0.0, 0.0, -1.0));
+        assert_relative_eq!(c.eye, -Vector::z_axis());
+        assert_relative_eq!(c.normal, -Vector::z_axis());
+        assert!(!c.inside);
+
+        let r = Ray::new(Point::origin(), Vector::z_axis());
+        let i = Intersection::new(&s, 1.0);
+
+        let c = i.prepare_computations(&r);
+
+        assert_relative_eq!(c.point, Point::new(0.0, 0.0, 1.0));
+        assert_relative_eq!(c.eye, -Vector::z_axis());
+        assert_relative_eq!(c.normal, -Vector::z_axis());
+        assert!(c.inside);
     }
 
     #[test]
