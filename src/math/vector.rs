14555f142907cced6a4fc167d307de2f604fd05b
Megan Pycroft <megan.pycroft@gmail.com>
2023-12-04 11:19:05 +1100

    Add cross function to Vector

--- src/math/vector.rs
+++ src/math/vector.rs
@@ -36,6 +36,14 @@ impl Vector {
     pub fn dot(&self, rhs: Self) -> f64 {
         self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
     }
+
+    pub fn cross(&self, rhs: Self) -> Self {
+        Self::new(
+            self.y * rhs.z - self.z * rhs.y,
+            self.z * rhs.x - self.x * rhs.z,
+            self.x * rhs.y - self.y * rhs.x,
+        )
+    }
 }
 
 impl Add for Vector {
@@ -189,6 +197,15 @@ mod tests {
 
         assert_approx_eq!(v1.dot(v2), d);
         assert_approx_eq!(v2.dot(v1), d);
+    }
+
+    #[test]
+    fn computing_the_cross_product_of_two_vectors() {
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        let v2 = Vector::new(2.0, 3.0, 4.0);
+
+        assert_approx_eq!(v1.cross(v2), Vector::new(-1.0, 2.0, -1.0));
+        assert_approx_eq!(v2.cross(v1), Vector::new(1.0, -2.0, 1.0));
     }
 
     #[test]
