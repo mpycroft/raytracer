341a026e532eb02fc9a25ba773a9e934e5593505
Megan Pycroft <megan.pycroft@gmail.com>
2023-12-22 12:15:03 +1100

    Add parallel ray intersect test to Plane

--- src/shape/plane.rs
+++ src/shape/plane.rs
@@ -1,6 +1,6 @@
 use crate::{
     intersection::{Intersectable, ListBuilder},
-    math::{Point, Ray, Vector},
+    math::{float::approx_eq, Point, Ray, Vector},
 };
 
 /// A `Plane` is an infinitely large plane situated along the x and z axes.
@@ -9,6 +9,10 @@ pub struct Plane;
 
 impl Intersectable for Plane {
     fn intersect<'a>(&'a self, ray: &Ray) -> Option<ListBuilder<'a>> {
+        if approx_eq!(ray.direction.y, 0.0) {
+            return None;
+        }
+
         todo!()
     }
 
@@ -21,6 +25,19 @@ impl Intersectable for Plane {
 mod tests {
     use super::*;
     use crate::math::float::*;
+
+    #[test]
+    fn intersect_with_a_ray_parallel_to_the_plane() {
+        let p = Plane;
+
+        assert!(p
+            .intersect(&Ray::new(Point::new(0.0, 10.0, 0.0), Vector::z_axis()))
+            .is_none());
+
+        assert!(p
+            .intersect(&Ray::new(Point::origin(), Vector::z_axis()))
+            .is_none());
+    }
 
     #[test]
     fn the_normal_of_a_plane_is_constant_everywhere() {
