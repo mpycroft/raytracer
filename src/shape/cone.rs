6ecda5132c979351320338927c376a6a0f85eacf
Megan Pycroft <megan.pycroft@gmail.com>
2024-01-22 11:59:41 +1100

    Add intersection code for Cone end caps

--- src/shape/cone.rs
+++ src/shape/cone.rs
@@ -3,7 +3,10 @@ use float_cmp::{ApproxEq, F64Margin};
 
 use crate::{
     intersection::TList,
-    math::{float::approx_eq, Point, Ray, Vector},
+    math::{
+        float::{approx_eq, approx_ne},
+        Point, Ray, Vector,
+    },
 };
 
 // A `Cone` is a double napped cone centred on the origin and extending in both
@@ -29,44 +32,72 @@ impl Cone {
         let c =
             ray.origin.x.powi(2) - ray.origin.y.powi(2) + ray.origin.z.powi(2);
 
-        if approx_eq!(a, 0.0) {
-            if approx_eq!(b, 0.0) {
-                return None;
-            }
-
-            return Some(TList::from(vec![-c / (2.0 * b)]));
-        }
-
-        let discriminant = b.powi(2) - 4.0 * a * c;
-
-        if discriminant < 0.0 {
-            return None;
-        };
-
-        let discriminant = discriminant.sqrt();
-        let a = 2.0 * a;
-
-        let t0 = (-b - discriminant) / a;
-        let t1 = (-b + discriminant) / a;
-
         let mut list = TList::new();
 
-        let y0 = ray.origin.y + t0 * ray.direction.y;
-        if self.minimum < y0 && y0 < self.maximum {
-            list.push(t0);
+        if approx_eq!(a, 0.0) {
+            if approx_ne!(b, 0.0) {
+                list.push(-c / (2.0 * b));
+            }
+        } else {
+            let discriminant = b.powi(2) - 4.0 * a * c;
+
+            if discriminant < 0.0 {
+                return None;
+            };
+
+            let discriminant = discriminant.sqrt();
+            let a = 2.0 * a;
+
+            let t0 = (-b - discriminant) / a;
+            let t1 = (-b + discriminant) / a;
+
+            let y0 = ray.origin.y + t0 * ray.direction.y;
+            if self.minimum < y0 && y0 < self.maximum {
+                list.push(t0);
+            }
+
+            let y1 = ray.origin.y + t1 * ray.direction.y;
+            if self.minimum < y1 && y1 < self.maximum {
+                list.push(t1);
+            }
         }
 
-        let y1 = ray.origin.y + t1 * ray.direction.y;
-        if self.minimum < y1 && y1 < self.maximum {
-            list.push(t1);
-        }
-
-        Some(list)
+        self.intersect_caps(ray, &mut list)
     }
 
     #[must_use]
     pub fn normal_at(&self, point: &Point) -> Vector {
         todo!()
+    }
+
+    #[must_use]
+    fn intersect_caps(&self, ray: &Ray, list: &mut TList) -> Option<TList> {
+        let check_cap = |t: f64, r: f64| {
+            let x = ray.origin.x + t * ray.direction.x;
+            let z = ray.origin.z + t * ray.direction.z;
+
+            x.powi(2) + z.powi(2) <= r.powi(2)
+        };
+
+        if self.closed && approx_ne!(ray.direction.y, 0.0) {
+            let t = (self.minimum - ray.origin.y) / ray.direction.y;
+
+            if check_cap(t, self.minimum) {
+                list.push(t);
+            }
+
+            let t = (self.maximum - ray.origin.y) / ray.direction.y;
+
+            if check_cap(t, self.maximum) {
+                list.push(t);
+            }
+        }
+
+        if list.is_empty() {
+            return None;
+        };
+
+        Some(list.to_owned())
     }
 }
 
@@ -89,7 +120,7 @@ impl ApproxEq for Cone {
 
 #[cfg(test)]
 mod tests {
-    use std::f64::INFINITY;
+    use std::f64::{consts::FRAC_1_SQRT_2, INFINITY};
 
     use super::*;
     use crate::math::float::*;
@@ -138,6 +169,37 @@ mod tests {
 
         assert_eq!(i.len(), 1);
         assert_approx_eq!(i[0], 0.353_55, epsilon = 0.000_01);
+    }
+
+    #[test]
+    fn intersecting_a_cones_end_caps() {
+        let c = Cone::new(-0.5, 0.5, true);
+
+        let i = c
+            .intersect(&Ray::new(Point::new(0.0, 0.0, -5.0), Vector::y_axis()));
+
+        assert!(i.is_none());
+
+        let i = c
+            .intersect(&Ray::new(
+                Point::new(0.0, 0.0, -0.25),
+                Vector::new(0.0, 1.0, 1.0).normalise(),
+            ))
+            .unwrap();
+
+        assert_eq!(i.len(), 2);
+        assert_approx_eq!(i[0], 0.088_39, epsilon = 0.000_01);
+        assert_approx_eq!(i[1], FRAC_1_SQRT_2, epsilon = 0.000_01);
+
+        let i = c
+            .intersect(&Ray::new(Point::new(0.0, 0.0, -0.25), Vector::y_axis()))
+            .unwrap();
+
+        assert_eq!(i.len(), 4);
+        assert_approx_eq!(i[0], 0.25);
+        assert_approx_eq!(i[1], -0.25);
+        assert_approx_eq!(i[2], -0.5);
+        assert_approx_eq!(i[3], 0.5);
     }
 
     #[test]
