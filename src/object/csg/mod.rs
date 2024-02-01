f7b70aa8bd67278e1b75d29e7b76ac124ac95043
Megan Pycroft <megan.pycroft@gmail.com>
2024-02-01 16:50:10 +1100

    Add intersection_allowed function to Csg and add Union tests

--- src/object/csg/mod.rs
+++ src/object/csg/mod.rs
@@ -17,6 +17,22 @@ impl Csg {
     pub fn new(operation: Operation, left: Object, right: Object) -> Self {
         Self { operation, left: Box::new(left), right: Box::new(right) }
     }
+
+    #[must_use]
+    fn intersection_allowed(
+        operation: Operation,
+        left_hit: bool,
+        in_left: bool,
+        in_right: bool,
+    ) -> bool {
+        match operation {
+            Operation::Difference => todo!(),
+            Operation::Intersection => todo!(),
+            Operation::Union => {
+                (left_hit && !in_right) || (!left_hit && !in_left)
+            }
+        }
+    }
 }
 
 impl ApproxEq for Csg {
@@ -46,5 +62,21 @@ mod tests {
         assert_eq!(c.operation, Operation::Union);
         assert_approx_eq!(c.left, &l);
         assert_approx_eq!(c.right, &r);
+    }
+
+    #[test]
+    fn evaluating_the_rules_for_a_csg_operation() {
+        let test_union = |l_hit, in_l, in_r| {
+            Csg::intersection_allowed(Operation::Union, l_hit, in_l, in_r)
+        };
+
+        assert!(!test_union(true, true, true));
+        assert!(test_union(true, true, false));
+        assert!(!test_union(true, false, true));
+        assert!(test_union(true, false, false));
+        assert!(!test_union(false, true, true));
+        assert!(!test_union(false, true, false));
+        assert!(test_union(false, false, true));
+        assert!(test_union(false, false, false));
     }
 }
